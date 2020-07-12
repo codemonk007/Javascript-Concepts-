daa45687dcec7904a2e2eb8587b143e27a027542
ganabhat <ganabhat@cisco.com>
2020-07-12 21:01:22 +0530

    str-number

---
+++ Javascript_concepts/StringifyNumber_convertStringToNumber.js
@@ -0,0 +1,25 @@
+let test=12;
+let test2 = String(test)
+let test3 = test.toString()
+let test4 = ""+test;
+let test5 = test+"";
+
+console.log(typeof test);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+
+
+let temp ="66"
+let temp1 = +temp;
+let temp2 = parseInt(temp)
+console.log(typeof temp);
+console.log(typeof temp1);
+console.log(typeof temp2);
+
+
+
+
+
+
